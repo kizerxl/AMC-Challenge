677894d02e76f92711e12ed103b63055fcfbb296
Felix Changoo <fchangoo@felixs-macbook-pro-2.local>
2018-10-24 23:08:02 -0400

    Initial Commit

---
+++ ShudderChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShudderChallenge
+//
+//  Created by Felix Changoo on 10/24/18.
+//  Copyright © 2018 Felix Changoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
